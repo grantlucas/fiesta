eee7ca987e8907bcf04b6efd85a0a9998bc41d41
Grant Lucas <contact@grantlucas.com>
2016-02-21 13:13:27 -0500

    Corrected arguments to have proper required flag

--- src/Command/BuildCommand.php
+++ src/Command/BuildCommand.php
@@ -15,37 +15,20 @@ class BuildCommand extends Command
         $this->setName('build')
         ->setDescription('Build your Fiesta photo essay.')
         ->addArgument('source',
-            InputOption::VALUE_REQUIRED,
+            InputArgument::REQUIRED,
             'The source folder with your original images.'
         )
         ->addArgument('destination',
-            InputOption::VALUE_REQUIRED,
+            InputArgument::REQUIRED,
             'The destination folder where the site will be built.'
         );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-            $input->validate();
-        /*try {
-            $input->validate();
-        } catch (Exception $e) {
-            $output->writeln($e->getMessage());
-        }*/
-
         $source = $input->getArgument('source');
         $destination = $input->getArgument('destination');
-        var_dump($source);
-        var_dump($destination);
 
-        /*if (empty($source)) {
-            throw new \InvalidArgumentException('Source required');
-        }
-
-        if (empty($destination)) {
-            throw new \InvalidArgumentException('Destination required');
-        }
-*/
         $output->writeln('Build that site');
         $output->writeln('Source: ' . $source);
         $output->writeln('Destination: ' . $destination);
