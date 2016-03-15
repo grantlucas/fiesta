4678c6b6caf95979182f1511fdd05747591e2b95
Grant Lucas <contact@grantlucas.com>
2016-03-14 22:08:30 -0400

    set up TODO's for what to work on next

--- src/Command/BuildCommand.php
+++ src/Command/BuildCommand.php
@@ -36,9 +36,11 @@ class BuildCommand extends Command
         $output->writeln('Source:' . $source->getDir());
         $output->writeln('Destination:' . $destination->getDir());
 
-        //TODO: Prompt user for confirmation to continue
+        // Prompt user for confirmation to continue
         $questionHelper = $this->getHelper('question');
         $question = new ConfirmationQuestion('The folling command will overwrite the destination folder. Are you sure you want to continue?', false);
+
+        $output->writeln('');
 
         if (!$questionHelper->ask($input, $output, $question)) {
             return;
@@ -58,5 +60,31 @@ class BuildCommand extends Command
         if (!$destination->isWritable()) {
             throw new \RuntimeException("Destination directory is not writable");
         }
+
+        // Get the list of files from the source folder
+        $files = $source->getFiles();
+
+        //TODO: Set up destination's images folder
+
+        // Store which files have been processed
+        $processedFiles = array();
+
+        // Loop through files and process them
+        foreach ($files as $file) {
+            var_dump($file);
+
+            // Store that this file was processed
+            $processedFiles[] = $file;
+
+            //TODO: Look for coutnerpart file (markdown text)
+            //TODO: If counterpart found, add it to the processed files list to prevent processing it again
+            //TODO: Pass the files through Twig partial template which renders the list item
+            //TODO: Store the rendered HTML to later passing to page template
+            //TODO: Copy the image into the original images folder
+            //TODO: Resize the image and copy to small/medium/large images folders
+
+        }
+
+        //TODO: Take list of files (HTML) and pass to page template file
     }
 }
